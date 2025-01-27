09739eb0fa78d5a55f69d09cf526d23ea4a5d504
Yasmim Sampaio Martins <53661815857@>
2025-01-27 15:28:54 -0300

    versão11.0

--- EXERCICIOS 23.01.2025/exercicio21.py
+++ EXERCICIOS 23.01.2025/exercicio21.py
@@ -1,34 +1,55 @@
-#exercicio 21
+class Cliente: 
+    produtos_disponiveis = []  
 
-class Clientes ():
-  def __init__(self,nome,idade,genero,estado_civil,data_nascimento) :
-    self.nome = nome
-    self.idade = idade
-    self.genero = genero
-    self.estado_civil = estado_civil
-    data_nascimento = data_nascimento
-    self.historico_compras = []
-    self.produtos_disponiveis = [Produtos.nome()]
-    self.carrinho = {}
-  def comprar (self,produto):
-    self.produto = produto
-    if self.produto in self.carrinho :
-      self.historico_compras.append(produto)
+    def __init__(self, nome, idade, genero, estado_civil, data_nascimento):
+        self.nome = nome
+        self.idade = idade
+        self.genero = genero
+        self.estado_civil = estado_civil
+        self.data_nascimento = data_nascimento
+        self.historico_compras = []
+        self.carrinho = {}
 
-      self.carrinho.remove(pop)
-      
-  def adicionar_ao_carrinho(self,produto):
-    
-    
+    def comprar(self, produto):
+        if produto in self.carrinho:
+            try:
+                self.historico_compras.append(produto)
+                del self.carrinho[produto]
+                return 'Compra realizada com sucesso'
+            except:
+                return 'Erro ao realizar a compra'
+        else:
+            return 'esse produto não está disponível no carrinho'
 
-class Produtos ():
-  def __init__(self,nome,preco,quantidade_estoque) :
-    self.nome = nome
-    self.preco =  preco
-    self.quantidade_estoque = quantidade_estoque
+    def adicionar_ao_carrinho(self, produto, quantidade):
+        if produto in Cliente.produtos_disponiveis: 
+            try:
+                self.carrinho[produto] = {'nome': produto.nome, 'quantidade': quantidade}
+                print({self.carrinho[produto]})
+                return 'produto adicionado ao carrinho com sucesso'
+            except:
+                return 'erro ao adicionar produto ao carrinho'
+        else:
+            return 'Produto não está disponivel no catalogo'
 
-class Pedidos ():
-  def __init__(self) -> None:
-    self.clientes = Clientes().nome()
-    self.produtos = Produtos().nome().clientes()
-    
+class Produto:
+    def __init__(self, nome, preco, quantidade_estoque):
+        self.nome = nome
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+       
+        Cliente.produtos_disponiveis.append(self)
+
+
+produto1 = Produto("camisa", 50.0, 100)
+produto2 = Produto("Calça", 120.0, 50)
+produto3 = Produto("tenis", 180.0, 30)
+
+
+cliente = Cliente('yasmim', 17, 'feminino', 'solteira', '31/08/2007')
+
+
+print(cliente.adicionar_ao_carrinho(produto1, 10)) 
+print(cliente.adicionar_ao_carrinho(produto2, 2))  
+
+print(cliente.comprar(produto1))  
